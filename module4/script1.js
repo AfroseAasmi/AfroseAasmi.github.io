ab3438d44323a8fcc0611e3c207c34671839fe60
AfroseAasmi <100026907+AfroseAasmi@users.noreply.github.com>
2023-07-22 11:50:35 +0530

    Add files via upload

---
+++ module4/script1.js
@@ -0,0 +1,16 @@
+(function () {
+
+    var names = ["Ann", "John", "Jenny", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    
+    for (var i = 0; i < names.length; i++) {
+    
+      var firstLetter = names[i].charAt(0).toLowerCase();
+      if (firstLetter === 'j') {
+        byeSpeaker.speak(names[i]);
+      } 
+      else {
+        helloSpeaker.speak(names[i]);
+      }
+    }
+    
+    })();
