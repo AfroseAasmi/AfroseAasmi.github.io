90868ee77a493ec97de1a832a141959c73bbbcc6
AfroseAasmi <100026907+AfroseAasmi@users.noreply.github.com>
2023-12-25 18:20:19 +0530

    Create contact.php

---
+++ college_management/contact.php
@@ -0,0 +1,20 @@
+<?php include("headd.html")
+?>
+<body>
+<div class="contact">
+<div class="add">
+<address>
+<br><br>
+Old Mahabalipuram Road, <br>
+Kamaraj Nagar, Semmancheri, <br>
+Chennai, Tamil Nadu 600119 <br>
+Phone No: 9934566789 <br>
+Mail: abcengg123@gmail.com </address>
+</div>
+<div class="map">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.5877557621466!2d80.21623851408891!3d12.869881720585985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a525bbeb0b2eeb7%3A0x2cb8fc0ae9b16ed5!2sSt%20Joseph&#39;s%20College%20Of%20Engineering!5e0!3m2!1sen!2sin!4v1671692556008!5m2!1sen!2sin" width="1000" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
+</div>
+</body>
+<?php include("footerr.html")
+?>
