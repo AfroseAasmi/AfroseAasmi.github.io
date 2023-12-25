3a2b9c1acc28bd3a26eb0cd7ad13638c49799d10
AfroseAasmi <100026907+AfroseAasmi@users.noreply.github.com>
2023-12-25 18:21:40 +0530

    Create about.php

---
+++ college_management/about.php
@@ -0,0 +1,37 @@
+<?php include("headd.html")
+?>
+<body>
+<div class="about">
+<div class="title">
+<h1>COURSES OFFERED</h1>
+<div class="work">
+<p>Courses offered<br>
+1. Computer Science and Engineering <br>
+2. Electronics and Communication Engineering <br>
+3. Electrical and Electronics Engineering <br>
+4. Civil Engineering <br>
+5. Chemical Engineering <br>
+6. Electrical and Instrumentation Engineering <br>
+7. Biotechnology <br>
+8. Information Technology <br>
+9. Mechanical Engineering <br>
+</p>
+</div>
+<div class="new">
+<div class="img5"><marquee direction="left">
+<img src="images/clg img7.avif" height="220" width="500" /></marquee>
+</div>
+<div class="img6"><marquee direction="right">
+<img src="images/clg img8.jpg" height="220" width="500" /></marquee>
+</div>
+<div class="img3"><marquee direction="right">
+<img src="images/clg img5.jpg" height="220" width="500" /></marquee>
+</div>
+<div class="img4"><marquee direction="left">
+<img src="images/clg img6.webp" height="220" width="500" /></marquee>
+</div>
+</div>
+</div>
+</body>
+<?php include("footerr.html")
+?>
