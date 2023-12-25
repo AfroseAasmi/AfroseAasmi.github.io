e721c0cef5d1029354598a6c4b3c7d27d576ae43
AfroseAasmi <100026907+AfroseAasmi@users.noreply.github.com>
2023-12-25 18:10:07 +0530

    Create home.php

---
+++ college_management/home.php
@@ -0,0 +1,26 @@
+<?php include("headd.html")
+?>
+<body>
+<div class="home">
+<div class="heading">
+<h1>ST. JOSEPH'S COLLEGE OF ENGINEERING</h1>
+<div class="working">
+<p>St. Joseph's College Of Engineering is a private engineering college established in 1990 at chennai, Tamil Nadu, India. It is an autonomous college affiliated to Anna University . The College offers BE/B.Tech at the undergraduate level and ME/M.Tech at the postgraduate level. The college has excellent academic and student centric facilities. Almost 200+ companies visit the college and 95% of the students get recruited every year with an average salary of 5.61 lakhs.
+</p>
+</div>
+<div class="img1">
+<img src="images/clg img1.jpg" height="220" width="400" />
+</div>
+<div class="img2">
+<img src="images/clg img2.jpeg" height="220" width="400" />
+</div>
+<div class="img3">
+<img src="images/clg img3.jpg" height="220" width="400" />
+</div>
+<div class="img4">
+<img src="images/clg img4.jpeg" height="220" width="400" />
+</div>
+</div>
+</body>
+<?php include("footerr.html")
+?>
